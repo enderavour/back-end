9a37980232dff4a36ad5f6aa4ad4712e0e08f936
enderavour <vovaboba1357@gmail.com>
2026-05-29 11:19:32 +0300

    BE #3: Applied requested changes

--- app/core/database.py
+++ app/core/database.py
@@ -2,9 +2,9 @@ from os import getenv
 
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
-DATABASE_URL = getenv("DATABASE_URL")
+from app.core.config import settings
 
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(settings.DATABASE_URL)
 
 AsyncSessionLocal = async_sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False
--- app/core/redis.py
+++ app/core/redis.py
@@ -2,7 +2,9 @@ from os import getenv
 
 import redis.asyncio as redis
 
-REDIS_HOST = getenv("REDIS_HOST")
-REDIS_PORT = getenv("REDIS_PORT")
+from app.core.config import settings
+
+REDIS_HOST = settings.REDIS_HOST
+REDIS_PORT = settings.REDIS_PORT
 
 redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
